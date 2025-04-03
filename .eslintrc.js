afeb9d18c010e0cc2615781d52a708efd781d0ee
Anil Murty <anil.murty@gmail.com>
2025-04-03 11:37:16 -0700

    fix: Update ESLint config to handle unescaped entities in both files

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   extends: 'next/core-web-vitals',
   overrides: [
     {
-      files: ['src/components/layout/Hero.tsx'],
+      files: ['src/components/layout/Hero.tsx', 'src/app/page.tsx'],
       rules: {
         'react/no-unescaped-entities': 'off'
       }
